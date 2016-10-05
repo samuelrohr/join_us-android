1c785027a54edf240138221e79cf8fcee0fad973
Samuel Rohr <samuel.scherrer@gmail.com>
2016-10-04 21:36:31 -0300

    -Criado swipe to refresh -Criado handler para request httpGet da api do twitch. -Criado cliend ID para acessar a api to twitch -Obtendo Json com os 50 top games e seus dados

--- app/src/main/java/br/com/join_us_android/MainActivity.java
+++ app/src/main/java/br/com/join_us_android/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.join_us_android;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -8,22 +9,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 import adapters.GameListAdapter;
+import httpUtil.HttpGetRequestHandler;
 import modules.GameInfo;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HttpGetRequestHandler.HttpGetCompleted {
 
     private ListView listView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private static final String TOP_GAMES_URL = "https://api.twitch.tv/kraken/games/top";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView) findViewById(R.id.listView);
-
-        listView.setAdapter(new GameListAdapter(this.getApplicationContext(), mockData()));
+        loadComponents();
     }
 
+    private void loadComponents() {
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new GameListAdapter(this.getApplicationContext(), mockData()));
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+        });
+    }
+
+    private void refreshData() {
+        HttpGetRequestHandler httpGetRequestHandler = new HttpGetRequestHandler(TOP_GAMES_URL, this);
+        httpGetRequestHandler.execute();
+    }
+
+    @Override
+    public void onHttpGetCompleted(String s) {
+        //TODO passar para o parser de JSON e montar os objetos a serem mostrados na lista
+        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    //TODO remover
     private List<GameInfo> mockData() {
         List<GameInfo> list = new ArrayList<>();
         list.add(new GameInfo("teste", "nada", 10, 20));
---
+++ app/src/main/java/httpUtil/HttpGetRequestHandler.java
@@ -0,0 +1,77 @@
+package httpUtil;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Rohr on 10/4/2016.
+ */
+public class HttpGetRequestHandler extends AsyncTask<String, Void, String> {
+
+    private String url = "";
+    private HttpGetCompleted listener;
+    private static final String CLIENT_ID = "m7d3djff8ihb50r5tykr8zw5g2zijr7";
+
+    public HttpGetRequestHandler(String url, HttpGetCompleted listener) {
+        super();
+
+        this.url = url;
+        this.listener = listener;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        listener.onHttpGetCompleted(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        try {
+            URL url = new URL(this.url);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Client-ID", CLIENT_ID);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setUseCaches(false);
+
+            urlConnection.connect();
+
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Get Response
+                InputStream is = urlConnection.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                String line;
+                StringBuffer response = new StringBuffer();
+                while ((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+                return response.toString();
+            } else {
+                return "";
+            }
+
+        } catch (IOException e) {
+            Log.e("GetGameInfoError", e.getMessage());
+        }
+        return null;
+    }
+
+    public interface HttpGetCompleted {
+        void onHttpGetCompleted(String s);
+    }
+}
